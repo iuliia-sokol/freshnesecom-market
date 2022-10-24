d04256f82c0c2f4511461b561fa3277586d1bf65
Iuliia Sokol <avare779@gmail.com>
2022-10-24 18:39:53 +0300

    Edited JS for dropdown menus, opacity transition added

--- src/javascript/header-dropdown-menus.js
+++ src/javascript/header-dropdown-menus.js
@@ -2,13 +2,18 @@ const menuItemEl = document.querySelectorAll('.menu__item');
 const subMenuEl = document.querySelectorAll('.submenu');
 const subSubMenuEl = document.querySelectorAll('.sub-submenu');
 
-console.log(menuItemEl);
 menuItemEl.forEach(el => el.addEventListener('mouseover', onOpenSubMenu));
 
 function onOpenSubMenu(event) {
   event.preventDefault();
   event.currentTarget.classList.add('menu__link--active');
   event.currentTarget.lastElementChild.ariaExpanded = true;
+
+  menuItemEl.forEach(el => {
+    if (el !== event.currentTarget) {
+      el.classList.add('blur-text');
+    }
+  });
 }
 
 menuItemEl.forEach(el => el.addEventListener('mouseout', onCloseSubMenu));
@@ -16,13 +21,17 @@ menuItemEl.forEach(el => el.addEventListener('mouseout', onCloseSubMenu));
 function onCloseSubMenu(event) {
   event.currentTarget.classList.remove('menu__link--active');
   event.currentTarget.lastElementChild.ariaExpanded = false;
+  menuItemEl.forEach(el => {
+    if (el !== event.currentTarget) {
+      el.classList.remove('blur-text');
+    }
+  });
 }
 
 subSubMenuEl.forEach(el => el.addEventListener('mouseenter', onOpenSubSubMenu));
 
 function onOpenSubSubMenu(event) {
   event.preventDefault();
-  console.dir(event.currentTarget.parentElement);
   event.currentTarget.parentElement.classList.add('submenu__item--active');
   event.currentTarget.ariaExpanded = true;
 }
