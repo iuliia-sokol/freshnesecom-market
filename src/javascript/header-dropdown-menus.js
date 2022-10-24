fea79150d18a540bbe7e5237595a976937155b41
Iuliia Sokol <avare779@gmail.com>
2022-10-24 16:45:07 +0300

    Dropdown lists JS edited

--- src/javascript/header-dropdown-menus.js
+++ src/javascript/header-dropdown-menus.js
@@ -1,32 +1,37 @@
-const arrowEl = document.querySelectorAll('.menu__icon');
 const menuItemEl = document.querySelectorAll('.menu__item');
 const subMenuEl = document.querySelectorAll('.submenu');
 const subSubMenuEl = document.querySelectorAll('.sub-submenu');
 
 console.log(menuItemEl);
-menuItemEl.forEach(el =>
-  el.addEventListener('mouseover', event => {
-    event.preventDefault();
-    event.currentTarget.classList.add('menu__link--active');
-    //   let arrow = event.currentTarget.getElementsByTagName('svg');
-    //   console.log(arrow)
-    // event.currentTarget.children[1].classList.add('menu__icon--active');
-  })
-);
+menuItemEl.forEach(el => el.addEventListener('mouseover', onOpenSubMenu));
+
+function onOpenSubMenu(event) {
+  event.preventDefault();
+  event.currentTarget.classList.add('menu__link--active');
+  event.currentTarget.lastElementChild.ariaExpanded = true;
+}
 
 menuItemEl.forEach(el => el.addEventListener('mouseout', onCloseSubMenu));
 
-// function onOpenSubMenu(event) {
-//   event.preventDefault();
-//   event.currentTarget.classList.add('menu__link--active');
-//   event.currentTarget.children.classList.add('menu__icon--active');
-
-//   console.log(event.currentTarget);
-//   //   subMenuEl.classList.toggle('.sub-submenu--visible');
-// }
-
 function onCloseSubMenu(event) {
-  event.preventDefault();
   event.currentTarget.classList.remove('menu__link--active');
-  console.log('mouseout');
+  event.currentTarget.lastElementChild.ariaExpanded = false;
+}
+
+subSubMenuEl.forEach(el => el.addEventListener('mouseenter', onOpenSubSubMenu));
+
+function onOpenSubSubMenu(event) {
+  event.preventDefault();
+  console.dir(event.currentTarget.parentElement);
+  event.currentTarget.parentElement.classList.add('submenu__item--active');
+  event.currentTarget.ariaExpanded = true;
+}
+
+subSubMenuEl.forEach(el =>
+  el.addEventListener('mouseleave', onCloseSubSubMenu)
+);
+
+function onCloseSubSubMenu(event) {
+  event.currentTarget.parentElement.classList.remove('submenu__item--active');
+  event.currentTarget.ariaExpanded = false;
 }
