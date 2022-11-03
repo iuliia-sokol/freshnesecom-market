1abc883fc5af8cfcc602a1b8f743f443992b8669
Iuliia Sokol <avare779@gmail.com>
2022-11-03 19:50:52 +0200

    some products added as data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './javascript/search_select';
-// import './javascript/products-data-editor';
-import './javascript/product-class-creator';
+
 import './javascript/products-markup-creator';
 import './javascript/header-dropdown-menus';
 import './javascript/product-cards-lightboxer';
